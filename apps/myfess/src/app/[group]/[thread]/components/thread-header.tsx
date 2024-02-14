bed8df06e643ef3a6574a157593b152e6e9b4e09
mimamch <mimamch28@gmail.com>
2024-02-14 18:24:18 +0700

    fix: router back while no history

--- apps/myfess/src/app/[group]/[thread]/components/thread-header.tsx
+++ apps/myfess/src/app/[group]/[thread]/components/thread-header.tsx
@@ -11,8 +11,17 @@ type Props = {
 export default function ThreadHeader(props: Props) {
   const router = useRouter();
   return (
-    <header className="border-b md:max-w-screen-sm flex items-center">
-      <button onClick={() => router.back()} className="p-4 mr-1">
+    <header className="border-b md:max-w-screen-sm flex items-center fixed top-0 w-full bg-background">
+      <button
+        onClick={() => {
+          if (window.history.length <= 2) {
+            router.push(`/`);
+            return;
+          }
+          router.back();
+        }}
+        className="p-4 mr-1"
+      >
         <ChevronLeftIcon className="w-6 h-6" />
       </button>
       {props.children}
